dcfcb7a5da7949a83fc347a70fdbaa098abcccd6
boris.ayupov <boris.ayupov@gmail.com>
2022-05-08 15:33:34 -0700

    custom primary navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,18 @@
 import "../sass/style.scss";
 window.addEventListener("load",()=>{
-  const mainItems = document.querySelectorAll(".su-main-nav li");
-  mainItems.forEach((item) => {
-    if (item.classList.contains("active")){
-      item.classList.add("su-main-nav__item--current");
+  const mainItems = document.querySelector(".block-system-main-menu > .block-content");
+  console.log(mainItems);
+  if(window.outerWidth<767){
+    mainItems.classList.add("su-main-nav");
+  }
+  window.addEventListener("resize",()=>{
+    if(window.outerWidth<767){
+      mainItems.classList.add("su-main-nav");
+    } else{
+      mainItems.classList.remove("su-main-nav");
     }
-  });
+  })
+
   const secItems = document.querySelectorAll(".su-secondary-nav__menu li a");
   secItems.forEach((item) => {
     // if (item.classList.contains("active-trail")){
